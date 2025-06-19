1c0b4a47501a1c6d4d9ea19345a22c35e1468981
lê Minh Nhựt <leminhnhut.9a10.2019@gmail.com>
2025-06-19 17:27:21 +0700

    chore: Force frontend redeploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
-
+// Dòng comment để ép deploy lại, ngày 19/06/2025
 const api = axios.create({
   baseURL: API_URL,
 });
