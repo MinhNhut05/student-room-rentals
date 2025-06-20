6d17fb83475b43697ff3fb57ed995992cae63e55
lê Minh Nhựt <leminhnhut.9a10.2019@gmail.com>
2025-06-20 11:40:11 +0700

    sửa để hiển thị phần HomePgae

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import AboutSection from "../../components/AboutSection/AboutSection";
@@ -7,20 +7,11 @@ import "./HomePage.scss";
 const HomePage = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (!user || !user.token) {
-      navigate("/login"); // Redirect to login if not authenticated
-    }
-  }, [navigate]);
-
   return (
     <div className="home-page container-home">
       <HeroSection />
       <AboutSection />
-      <div className="content-section">
-        
-      </div>
+      <div className="content-section"></div>
     </div>
   );
 };
